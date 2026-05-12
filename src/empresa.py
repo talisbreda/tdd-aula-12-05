2478a290fb9f9550f529cb6d62ce9b6b3e318b0b
Talis Breda <talisbreda@gmail.com>
2026-05-12 10:04:58 -0300

    Código inclusão funcionário na empresa

--- src/empresa.py
+++ src/empresa.py
@@ -1,3 +1,7 @@
 class Empresa:
     def __init__(self, nome):
         self.nome = nome
+        self.funcionarios = []
+
+    def incluir_funcionario(self, funcionario):
+        self.funcionarios.append(funcionario)
