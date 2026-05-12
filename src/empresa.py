677173d385f27872e4c8028ceab23118c4f5e835
Talis Breda <talisbreda@gmail.com>
2026-05-12 10:07:55 -0300

    Código inclusão projeto na empresa

--- src/empresa.py
+++ src/empresa.py
@@ -2,6 +2,10 @@ class Empresa:
     def __init__(self, nome):
         self.nome = nome
         self.funcionarios = []
+        self.projetos = []
 
     def incluir_funcionario(self, funcionario):
         self.funcionarios.append(funcionario)
+
+    def incluir_projeto(self, projeto):
+        self.projetos.append(projeto)
