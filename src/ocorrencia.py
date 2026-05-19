81fdcef05d124358e9b9bbb62028421614deed11
Talis Breda <talisbreda@gmail.com>
2026-05-19 10:01:45 -0300

    Código identificador único

--- src/ocorrencia.py
+++ src/ocorrencia.py
@@ -1,8 +1,14 @@
 class Ocorrencia:
-    funcionario = None
+    id = 0
 
     def __init__(self, nome):
         self.nome = nome
+        self.funcionario = None
+        self.id = self.incrementa_contador()
+
+    def incrementa_contador(contador):
+        Ocorrencia.id += 1
+        return Ocorrencia.id
 
     def atribuir_funcionario(self, funcionario):
         self.funcionario = funcionario
